ecbc95823421170f642c1423556c30de780e23a5
Konrad Szwarc <konrad.szwarc.dev@gmail.com>
2022-12-29 12:22:53 +0100

    Add Tailwind CSS

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ const Home = () => (
     </Head>
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
+        <p className="text-md">
           Get started by editing&nbsp;
           <code className={styles.code}>pages/index.tsx</code>
         </p>
