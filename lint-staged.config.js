fafdba63cedbab29b168c691087ca7ad1cdc0df2
Konrad Szwarc <konrad.szwarc.dev@gmail.com>
2023-01-01 18:30:17 +0100

    Add pre-commit tests for files affected by changes

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   '*': 'prettier --write --ignore-unknown',
   '*.{ts,tsx,js}': 'eslint --fix',
+  '*.{ts,tsx,json}': 'jest --findRelatedTests',
   '*.{ts,tsx}': () => 'tsc --noEmit',
 };
